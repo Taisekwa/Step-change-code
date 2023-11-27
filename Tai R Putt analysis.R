1b652f55becf0db365c83da0350c175644e124f7
Taisekwa Chikazhe <taisekwa.chikazhe@dairynz.co.nz>
2023-11-27 15:11:15 +1300

    Adding Putt, step change code.

---
+++ Tai R Putt analysis.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(reshape2)
+library(dplyr)
+
+# Read data from file
+data <- read.csv("C:/Users/chikazhet/putt1.csv")
+
+#Inspect the data
+#Inspect the data
+str(data)
+# filtering data based on season
+temp1 <- filter(data, season == "2021-22")
+
+temp2 <- filter(temp1, district == "South Waikato")
+
+
+
+# Filtering based on profit above or below median 2021/22 season
+LowOP <- filter(temp1, Operating_profit_ha <= median(temp1$Operating_profit_ha))
+HighOP <- filter(temp1, Operating_profit_ha > median(temp1$Operating_profit_ha))
+
+#binding the filtered data and creating a Profitability column
+ding <-  rbind(HighOP %>% mutate(Profitability = "A"), 
+LowOP %>% mutate(Profitability = "B"))
+
+
+#Selecting the variables we need
+ding2 <- select(ding,season,Farm,Operating_profit_ha,Farm_working_expenses_per_kgMS,MS_per_ha,methane,total_feed_eaten_tha,pasture_and_crop_eaten_per_ha,N_surplus,N_fertiliser_per_ha,imported_supplements_eaten_tha, Profitability, milking_interval)
+
+# Presenting the data into a long format
+molten.data <- melt(ding2, id = c("Profitability","milking_interval", "season","Farm"))
+
+
+
+
+# Plotting data on multiple-variables 2021-22
+p2 <- ggplot(molten.data, aes(x=Profitability, y = value, fill=Profitability, label=Farm)) + 
+  geom_boxplot() +
+  theme_gray() +
+  #geom_jitter() +
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold", size=2) +
+  theme(legend.position = "none")+
+  facet_wrap(~variable,scales = "free",labeller = as_labeller(
+    c(Operating_profit_ha = "Operating profit $/ha", Farm_working_expenses_per_kgMS = "Farm working expenses $/kgMS",MS_per_ha ="Milksolids kg/ha", methane ="Methane t CO2 equiv/ha",total_feed_eaten_tha = "Total feed eaten t DM/ha",pasture_and_crop_eaten_per_ha="Pasture and crop eaten t DM/ha", N_surplus="N surplus kg N/ha",N_fertiliser_per_ha = "N fertiliser kg N/ha", imported_supplements_eaten_tha = "Imported supplements eaten t DM/ha" ))) +
+  labs(x = NULL, y = NULL) +
+  labs(title="2021-22 season(Waikato Region-189 farms) A= Operating profit above, B= below median") +
+  theme(axis.title.x = element_text(face="bold", size=10),axis.text.x  = element_text(size=8)) +
+  theme(axis.title.y = element_text(face="bold", size=10),axis.text.y  = element_text(size=8)) +
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5)) 
+p2
+
+
+
+# Filtering based on profit above or below median 2021/22 season South Waikato district
+LowOP <- filter(temp2, Operating_profit_ha <= median(temp1$Operating_profit_ha))
+HighOP <- filter(temp2, Operating_profit_ha > median(temp1$Operating_profit_ha))
+
+#binding the filtered data and creating a Profitability column
+ding <-  rbind(HighOP %>% mutate(Profitability = "A"), 
+               LowOP %>% mutate(Profitability = "B"))
+
+
+#Selecting the variables we need
+ding2 <- select(ding,season,Farm,Operating_profit_ha,Farm_working_expenses_per_kgMS,MS_per_ha,methane,total_feed_eaten_tha,pasture_and_crop_eaten_per_ha,N_surplus,N_fertiliser_per_ha,imported_supplements_eaten_tha, Profitability, milking_interval)
+
+# Presenting the data into a long format
+molten.data <- melt(ding2, id = c("Profitability","milking_interval", "season","Farm"))
+
+
+
+
+# Plotting data on multiple-variables 2021-22
+p2 <- ggplot(molten.data, aes(x=Profitability, y = value, fill=Profitability, label=Farm)) + 
+  geom_boxplot() +
+  theme_gray() +
+  #geom_jitter() +
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold", size=2) +
+  theme(legend.position = "none")+
+  facet_wrap(~variable,scales = "free",labeller = as_labeller(
+    c(Operating_profit_ha = "Operating profit $/ha", Farm_working_expenses_per_kgMS = "Farm working expenses $/kgMS",MS_per_ha ="Milksolids kg/ha", methane ="Methane t CO2 equiv/ha",total_feed_eaten_tha = "Total feed eaten t DM/ha",pasture_and_crop_eaten_per_ha="Pasture and crop eaten t DM/ha", N_surplus="N surplus kg N/ha",N_fertiliser_per_ha = "N fertiliser kg N/ha", imported_supplements_eaten_tha = "Imported supplements eaten t DM/ha" ))) +
+  labs(x = NULL, y = NULL) +
+  labs(title="2021-22 season(South Waikato-41 farms) A= Operating profit above, B= below median") +
+  theme(axis.title.x = element_text(face="bold", size=10),axis.text.x  = element_text(size=8)) +
+  theme(axis.title.y = element_text(face="bold", size=10),axis.text.y  = element_text(size=8)) +
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5)) 
+p2
+
+
+
+
---
+++ Tai quandrant R Putt farm.R
@@ -0,0 +1,196 @@
+library(tidyverse)
+library(reshape2)
+library(dplyr)
+
+## Read data from file
+data <- read.csv("C:/Users/chikazhet/putt1.csv")
+
+##Inspect the data
+##Inspect the data
+str(data)
+
+# Filter 2021/22 season and subset profitability based on below or above median profit.
+
+data2 <- data %>%filter(data$district== "South Waikato")%>%
+  dplyr::mutate(Profitability = 
+                  dplyr::case_when(
+                    Operating_profit_ha > median(Operating_profit_ha) ~ "Above median",
+                    Operating_profit_ha <= median(Operating_profit_ha) ~ "Below median"))
+profit_drivers <- ggplot(data2, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
+  geom_point()
+profit_drivers
+data2$season<-  factor(data2$season)
+
+
+MedianOperating_profit_ha <- median(data2$Operating_profit_ha)
+
+
+#Construct the quadrant graphs for 2021/22 season
+
+ggplot(data2, aes(N_surplus,Operating_profit_ha, label=Farm)) +
+  
+  geom_point(size=1.5, color="red") +
+  
+  geom_hline(yintercept=median(data$Operating_profit_ha),
+                                                
+ col="blue") + geom_vline(xintercept=median(data$N_surplus), 
+                        
+  col="blue") + annotate("text", x = 0, y = 7500, label = "A", col = "blue", size = 10) +
+  
+  annotate("text", x = 250, y = 7500, label = "B", col = "blue", size = 10)+
+  
+  annotate("text", x = 0, y = 1250, label = "C", col = "blue", size = 10) +
+  
+  annotate("text", x = 250, y = 1250, label = "D", col = "blue", size = 10) +
+  
+  #annotate("text", x = -35, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
+
+  xlab("Purchased N surplus (kg N/ha/yr)") + ylab("Dairy operating profit ($/ha)") +
+  
+  labs(title="Operating profit vs purchased N surplus 2021-22 season") + 
+  
+  theme(plot.title=element_text(hjust=0.5)) + 
+  
+  theme(legend.position = "bottom") +
+  
+  theme(axis.title.x = element_text(face="bold", size=12),axis.text.x  = element_text(size=8)) +
+  
+  theme(axis.title.y = element_text(face="bold", size=12),axis.text.y  = element_text(size=8)) +
+  
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
+
+
+# methane quandrant graph with AAA, farm 1 and 2 positions.
+
+ggplot(data2, aes(methane,Operating_profit_ha,label=Farm)) +
+  
+  geom_point(size=1.5, col="red") +
+  
+  geom_hline(yintercept=median(data$Operating_profit_ha), col="blue") + 
+  
+  geom_vline(xintercept=median(data$methane), col="blue")+
+  
+  annotate("text", x = 5, y = 7500, label = "A", col = "blue", size = 10) +
+  
+  annotate("text", x = 12.5, y = 7500, label = "B", col = "blue", size = 10)+
+  
+  annotate("text", x = 5, y = 1250, label = "C", col = "blue", size = 10) +
+  
+  annotate("text", x = 12.5, y = 1250, label = "D", col = "blue", size = 10) +
+  
+  #annotate("text", x = 8.8, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold" ) +
+  
+  xlab("Methane emission (t CO2 equiv./ha/yr)") + 
+  
+  ylab("Dairy operating profit ($/ha)") + 
+  
+  labs(title="Operating profit vs methane emissions 2021-22 season") + 
+  
+  theme(plot.title=element_text(hjust=0.5)) + 
+  
+  theme(legend.position = "bottom") +
+  
+  theme(axis.title.x = element_text(face="bold", size=12),axis.text.x  = element_text(size=8)) +
+  
+  theme(axis.title.y = element_text(face="bold", size=12),axis.text.y  = element_text(size=8)) +
+  
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
+
+
+#Quadrant graph for the whole waikato
+
+tai <- data %>%
+  dplyr::mutate(Profitability = 
+                  dplyr::case_when(
+                    Operating_profit_ha > median(Operating_profit_ha) ~ "Above median",
+                    Operating_profit_ha <= median(Operating_profit_ha) ~ "Below median"))
+profit_drivers <- ggplot(datatai, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
+  geom_point()
+profit_drivers
+data$season<-  factor(data$season)
+
+
+MedianOperating_profit_ha <- median(tai$Operating_profit_ha)
+
+
+#Construct the quadrant graphs for 2021/22 season
+
+ggplot(tai, aes(N_surplus,Operating_profit_ha, label=Farm)) +
+  
+  geom_point(size=1.5, color="red") +
+  
+  geom_hline(yintercept=median(data$Operating_profit_ha),
+             
+             col="blue") + geom_vline(xintercept=median(data$N_surplus), 
+                                      
+                                      col="blue") + annotate("text", x = 0, y = 7500, label = "A", col = "blue", size = 10) +
+  
+  annotate("text", x = 250, y = 7500, label = "B", col = "blue", size = 10)+
+  
+  annotate("text", x = 0, y = 1250, label = "C", col = "blue", size = 10) +
+  
+  annotate("text", x = 250, y = 1250, label = "D", col = "blue", size = 10) +
+  
+  #annotate("text", x = -35, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
+  
+  xlab("Purchased N surplus (kg N/ha/yr)") + ylab("Dairy operating profit ($/ha)") +
+  
+  labs(title="Operating profit vs purchased N surplus 2021-22 season") + 
+  
+  theme(plot.title=element_text(hjust=0.5)) + 
+  
+  theme(legend.position = "bottom") +
+  
+  theme(axis.title.x = element_text(face="bold", size=12),axis.text.x  = element_text(size=8)) +
+  
+  theme(axis.title.y = element_text(face="bold", size=12),axis.text.y  = element_text(size=8)) +
+  
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
+
+
+# methane quandrant graph with AAA, farm 1 and 2 positions.
+
+ggplot(tai, aes(methane,Operating_profit_ha,label=Farm)) +
+  
+  geom_point(size=1.5, col="red") +
+  
+  geom_hline(yintercept=median(data$Operating_profit_ha), col="blue") + 
+  
+  geom_vline(xintercept=median(data$methane), col="blue")+
+  
+  annotate("text", x = 5, y = 7500, label = "A", col = "blue", size = 10) +
+  
+  annotate("text", x = 12.5, y = 7500, label = "B", col = "blue", size = 10)+
+  
+  annotate("text", x = 5, y = 1250, label = "C", col = "blue", size = 10) +
+  
+  annotate("text", x = 12.5, y = 1250, label = "D", col = "blue", size = 10) +
+  
+  #annotate("text", x = 8.8, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  
+  geom_text(aes(label=ifelse(Farm=="PF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold" ) +
+  
+  xlab("Methane emission (t CO2 equiv./ha/yr)") + 
+  
+  ylab("Dairy operating profit ($/ha)") + 
+  
+  labs(title="Operating profit vs methane emissions 2021-22 season") + 
+  
+  theme(plot.title=element_text(hjust=0.5)) + 
+  
+  theme(legend.position = "bottom") +
+  
+  theme(axis.title.x = element_text(face="bold", size=12),axis.text.x  = element_text(size=8)) +
+  
+  theme(axis.title.y = element_text(face="bold", size=12),axis.text.y  = element_text(size=8)) +
+  
+  theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
+
+
+
