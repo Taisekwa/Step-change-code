f281154deee51a82c982788001b97f9981567edf
Taisekwa Chikazhe <taisekwa.chikazhe@dairynz.co.nz>
2024-03-13 15:47:25 +1300

    improved

--- Tai quandrant R Gudeon farm.R
+++ Tai quandrant R Gudeon farm.R
@@ -3,7 +3,7 @@ library(reshape2)
 library(dplyr)
 
 ## Read data from file
-data <- read.csv("C:/Users/chikazhet/gudgeon.csv")
+data <- read.csv("C:/Users/chikazhet/waikato.csv")
 
 ##Inspect the data
 ##Inspect the data
@@ -11,7 +11,7 @@ str(data)
 
 # Filter 2021/22 season and subset profitability based on below or above median profit.
 
-data2 <- data %>% filter(data$season== "2021_22")%>%
+data2 <- data %>% filter(data$season== "2021-22")%>%
   dplyr::mutate(Profitability = 
                   dplyr::case_when(
                     Operating_profit_ha > median(Operating_profit_ha) ~ "Above median",
@@ -25,13 +25,11 @@ data2$season<-  factor(data2$season)
 MedianOperating_profit_ha <- median(data2$Operating_profit_ha)
 
 
-#Construct the quadrant graphs for 2021/22 season
+#Construct the quadrant graphs for 2021/22 season Waikato
 
 ggplot(data2, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   
   geom_point(size=1.5, color="red") +
-  
-  theme_gray() +
   
   geom_hline(yintercept=median(data$Operating_profit_ha),
                                                 
@@ -45,9 +43,7 @@ ggplot(data2, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   
   annotate("text", x = 250, y = 1250, label = "D", col = "blue", size = 10) +
   
-  #annotate("text", x = -35, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
-  
-  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
+   geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
 
   xlab("Purchased N surplus (kg N/ha/yr)") + ylab("Dairy operating profit ($/ha)") +
   
@@ -64,13 +60,11 @@ ggplot(data2, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
 
 
-# methane quandrant graph with AAA, farm 1 and 2 positions.
+# methane quandrant graph with AAA, farm 1 and 2 positions 2021/22 Waikato.
 
 ggplot(data2, aes(methane,Operating_profit_ha,label=Farm)) +
   
   geom_point(size=1.5, col="red") +
-  
-  theme_gray() +
   
   geom_hline(yintercept=median(data$Operating_profit_ha), col="blue") + 
   
@@ -105,32 +99,31 @@ ggplot(data2, aes(methane,Operating_profit_ha,label=Farm)) +
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
 
 
-# Filter 2020/21 season and subset profitability based on below or above median profit.
+#Quadrant graph for the South Waikato 2021/22
 
-data3 <- data %>% filter(data$season== "2020_21")%>%
+tai <- data2 %>% filter(data2$district== "South Waikato") %>%
   dplyr::mutate(Profitability = 
                   dplyr::case_when(
                     Operating_profit_ha > median(Operating_profit_ha) ~ "Above median",
                     Operating_profit_ha <= median(Operating_profit_ha) ~ "Below median"))
-profit_drivers <- ggplot(data3, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
+profit_drivers <- ggplot(datatai, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
   geom_point()
 profit_drivers
-data3$season<-  factor(data3$season)
+data$season<-  factor(data$season)
 
 
-MedianOperating_profit_ha <- median(data3$Operating_profit_ha)
+MedianOperating_profit_ha <- median(tai$Operating_profit_ha)
 
-#Construct the quadrant graphs for 2020/21 season
 
-ggplot(data3, aes(N_surplus,Operating_profit_ha, label=Farm)) +
+#Construct the quadrant graphs for  south Waikato 2021/22 season
+
+ggplot(tai, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   
   geom_point(size=1.5, color="red") +
   
-  theme_gray() +
-  
-  geom_hline(yintercept=median(data3$Operating_profit_ha),
+  geom_hline(yintercept=median(data$Operating_profit_ha),
              
-             col="blue") + geom_vline(xintercept=median(data3$N_surplus), 
+             col="blue") + geom_vline(xintercept=median(data$N_surplus), 
                                       
                                       col="blue") + annotate("text", x = 0, y = 7500, label = "A", col = "blue", size = 10) +
   
@@ -140,13 +133,13 @@ ggplot(data3, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   
   annotate("text", x = 250, y = 1250, label = "D", col = "blue", size = 10) +
   
-  #annotate("text", x = 45, y = 2561, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  #annotate("text", x = -35, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
   
-  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0, fontface="bold") +
+  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
   
   xlab("Purchased N surplus (kg N/ha/yr)") + ylab("Dairy operating profit ($/ha)") +
   
-  labs(title="Operating profit vs purchased N surplus 2020-21 season") + 
+  labs(title="Operating profit vs purchased N surplus 2021-22 season") + 
   
   theme(plot.title=element_text(hjust=0.5)) + 
   
@@ -159,17 +152,15 @@ ggplot(data3, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
 
 
-# methane quandrant graph with AAA.
+# South Waikato 2021/22 methane quandrant graph with AAA, farm 1 and 2 positions.
 
-ggplot(data3, aes(methane,Operating_profit_ha,label=Farm)) +
+ggplot(tai, aes(methane,Operating_profit_ha,label=Farm)) +
   
   geom_point(size=1.5, col="red") +
   
-  theme_gray() +
+  geom_hline(yintercept=median(data$Operating_profit_ha), col="blue") + 
   
-  geom_hline(yintercept=median(data3$Operating_profit_ha), col="blue") + 
-  
-  geom_vline(xintercept=median(data3$methane), col="blue")+
+  geom_vline(xintercept=median(data$methane), col="blue")+
   
   annotate("text", x = 5, y = 7500, label = "A", col = "blue", size = 10) +
   
@@ -179,7 +170,7 @@ ggplot(data3, aes(methane,Operating_profit_ha,label=Farm)) +
   
   annotate("text", x = 12.5, y = 1250, label = "D", col = "blue", size = 10) +
   
-  #annotate("text", x = 8.8, y = 2561, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
+  #annotate("text", x = 8.8, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
   
   geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold" ) +
   
@@ -187,7 +178,7 @@ ggplot(data3, aes(methane,Operating_profit_ha,label=Farm)) +
   
   ylab("Dairy operating profit ($/ha)") + 
   
-  labs(title="Operating profit vs methane emissions 2020-21 season") + 
+  labs(title="Operating profit vs methane emissions 2021-22 season") + 
   
   theme(plot.title=element_text(hjust=0.5)) + 
   
@@ -200,48 +191,51 @@ ggplot(data3, aes(methane,Operating_profit_ha,label=Farm)) +
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
 
 
-# Filter 2019/20 season and subset profitability based on below or above median profit.
+# Filter 2022/23 season and subset profitability based on below or above median profit.
 
-data4 <- data %>% filter(data$season== "2019_20")%>%
+data3 <- data %>% filter(data$season== "2022-23")%>%
   dplyr::mutate(Profitability = 
                   dplyr::case_when(
                     Operating_profit_ha > median(Operating_profit_ha) ~ "Above median",
                     Operating_profit_ha <= median(Operating_profit_ha) ~ "Below median"))
-profit_drivers <- ggplot(data4, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
+profit_drivers <- ggplot(data3, aes(x = methane, y = Operating_profit_ha, color=Profitability)) +
   geom_point()
 profit_drivers
-data4$season<-  factor(data4$season)
+data3$season<-  factor(data3$season)
+
+profit_drivers1 <- ggplot(data3, aes(x = N_surplus, y = Operating_profit_ha, color=Profitability)) +
+  geom_point()
+profit_drivers1
 
 
-MedianOperating_profit_ha <- median(data4$Operating_profit_ha)
+MedianOperating_profit_ha <- median(data3$Operating_profit_ha)
 
-#Construct the quadrant graphs for 2019/20 season
 
-ggplot(data4, aes(N_surplus,Operating_profit_ha,label=Farm)) +
+
+
+#Construct the quadrant graphs for 2022/23 season Waikato
+
+ggplot(data3, aes(N_surplus,Operating_profit_ha, label=Farm)) +
   
   geom_point(size=1.5, color="red") +
   
-  theme_gray() +
-  
-  geom_hline(yintercept=median(data4$Operating_profit_ha),
+  geom_hline(yintercept=median(data3$Operating_profit_ha),
              
-             col="blue") + geom_vline(xintercept=median(data4$N_surplus), 
+             col="blue") + geom_vline(xintercept=median(data$N_surplus), 
                                       
                                       col="blue") + annotate("text", x = 0, y = 7500, label = "A", col = "blue", size = 10) +
   
   annotate("text", x = 250, y = 7500, label = "B", col = "blue", size = 10)+
   
-  annotate("text", x = 0, y = -4000, label = "C", col = "blue", size = 10) +
+  annotate("text", x = 0, y = 1250, label = "C", col = "blue", size = 10) +
   
-  annotate("text", x = 250, y = -4000, label = "D", col = "blue", size = 10) +
+  annotate("text", x = 250, y = 1250, label = "D", col = "blue", size = 10) +
   
-  #annotate("text", x = 67, y = 2242, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
-  
-  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold" ) +
+  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold") +
   
   xlab("Purchased N surplus (kg N/ha/yr)") + ylab("Dairy operating profit ($/ha)") +
   
-  labs(title="Operating profit vs purchased N surplus 2019-20 season") + 
+  labs(title="Operating profit vs purchased N surplus 2022-23 season") + 
   
   theme(plot.title=element_text(hjust=0.5)) + 
   
@@ -254,35 +248,33 @@ ggplot(data4, aes(N_surplus,Operating_profit_ha,label=Farm)) +
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
 
 
-# methane quandrant graph with AAA.
+# methane quandrant graph with AAA, farm 1 and 2 positions 2022/23 Waikato.
 
-ggplot(data4, aes(methane,Operating_profit_ha, label=Farm)) +
+ggplot(data3, aes(methane,Operating_profit_ha,label=Farm)) +
   
   geom_point(size=1.5, col="red") +
   
-  theme_gray() +
+  geom_hline(yintercept=median(data3$Operating_profit_ha), col="blue") + 
   
-  geom_hline(yintercept=median(data4$Operating_profit_ha), col="blue") + 
+  geom_vline(xintercept=median(data$methane), col="blue")+
   
-  geom_vline(xintercept=median(data4$methane), col="blue")+
+  annotate("text", x = 5.5, y = 7500, label = "A", col = "blue", size = 10) +
   
-  annotate("text", x = 5, y = 7500, label = "A", col = "blue", size = 10) +
+  annotate("text", x = 15, y = 7500, label = "B", col = "blue", size = 10)+
   
-  annotate("text", x = 12.5, y = 7500, label = "B", col = "blue", size = 10)+
+  annotate("text", x = 5.5, y = 1250, label = "C", col = "blue", size = 10) +
   
-  annotate("text", x = 5, y = -4000, label = "C", col = "blue", size = 10) +
+  annotate("text", x = 15, y = 1250, label = "D", col = "blue", size = 10) +
   
-  annotate("text", x = 12.5, y = -4000, label = "D", col = "blue", size = 10) +
+  #annotate("text", x = 8.8, y = 4512, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
   
-  #annotate("text", x = 8.5, y = 2242, label = "Gudgeon", col = "black", size = 4, fontface="bold") +
-  
-  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0, fontface="bold") +
+  geom_text(aes(label=ifelse(Farm=="GF",as.character(Farm),'')),hjust=0,vjust=0,fontface="bold" ) +
   
   xlab("Methane emission (t CO2 equiv./ha/yr)") + 
   
   ylab("Dairy operating profit ($/ha)") + 
   
-  labs(title="Operating profit vs methane emissions 2019-20 season") + 
+  labs(title="Operating profit vs methane emissions 2022-23 season") + 
   
   theme(plot.title=element_text(hjust=0.5)) + 
   
@@ -293,6 +285,7 @@ ggplot(data4, aes(methane,Operating_profit_ha, label=Farm)) +
   theme(axis.title.y = element_text(face="bold", size=12),axis.text.y  = element_text(size=8)) +
   
   theme(axis.line.x = element_line(color="black", size = 0.5),axis.line.y = element_line(color="black", size = 0.5))
+
 
 
 
